2afe4582af992bd9fcd9ff987c5c2f55a042711e
JovianPlanet <davidpunk@gmail.com>
2021-10-20 00:00:57 -0500

    FLAIR

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 ## FCD in MRI features simulator
 
+This is an attempt at simulating Focal Cortical Dysplasias features on MRI volumes of healthy patients.
+
+	Clone this repository
+	Make directory named `temp_ds' inside this project's folder
+	Put the T1, FLAIR and White matter's mask in it
+	Run `main.py' 
+
+### Simulation by brightness increment of white matter on T2-FLAIR sequence
+
+Simulates brightness augmentation of the white matter on T2-FLAIR sequences. By extracting the white matter mask and selecting a random point in it, a squared region is extracted with center on such point. Thus, the simulated region, as of right now, is a square placed on a single slice of the sequence. The intensity of the pixel is increased by a factor of the pixel value, only the pixels that belong to white matter and are inside the square are considered for brightness increase.
+
+![alt text](1.png "ITM")
+
--- main.py
+++ main.py
@@ -31,44 +31,54 @@ tamano = 5
 intensidad = 1.3
 img_path = 'temp_ds'
 original = nib.load(os.path.join(img_path, 'T1.nii')).get_fdata()
-gm = nib.load(os.path.join(img_path, 'unet2D_predictions_GM.nii')).get_fdata()
-wm = nib.load(os.path.join(img_path, 'unet2D_predictions_WM.nii')).get_fdata()
+orig_flair = nib.load(os.path.join(img_path, 'T2_FLAIR.nii')).get_fdata()
+gm = nib.load(os.path.join(img_path, 'unet2D_predictions_GM.nii')).get_fdata() # Mascara de la materia gris
+wm = nib.load(os.path.join(img_path, 'unet2D_predictions_WM.nii')).get_fdata() # Mascara de la materia blanca
 
-ug, gm_ind = np.unique(gm, return_index=True)
-uw, wm_ind = np.unique(wm, return_index=True)
-print(gm_ind[0])
-print(wm_ind)
+ug, gm_ind = np.unique(gm, return_index=True) # Devuelve los indices de los pixeles pertenecientes a materia gris
+uw, wm_ind = np.unique(wm, return_index=True) # Devuelve los indices de los pixeles pertenecientes a materia blanca
 
 #plot_slice(original[:,:,24], original[:,:,24]*gm[:,:,24], original[:,:,24]*wm[:,:,24])
 #plot_slice(original[:,:,24], np.where(gm[:,:,24]==1, original[:,:,24], 0), np.where(wm[:,:,24]==1, original[:,:,24], 0))
 
-
 # coger al azar un punto de wm, senalarlo y aumentarle el brillo a este y a la region 
 
-x = np.copy(wm[:,:,24])
-#plot_slice(x, x, x)
+x = np.copy(wm[:,:,24]) # Un slice de materia blanca
+
 x_ind = np.argwhere(x==1) # Arreglo con los indices donde los pixeles pertenecen a la materia blanca
 
 point_ = x_ind[np.random.randint(0, x_ind.shape[0]),:] # Punto aleatorio de la materia blanca
-reg_wm_msk = x[point_[0]-tamano:point_[0]+tamano, point_[1]-tamano:point_[1]+tamano]
+
+# Con base en l punto seleccionado se extrae una region de materia blanca (cuadro de area = 2tamano x 2tamano)
+reg_wm_msk = x[point_[0]-tamano:point_[0]+tamano, point_[1]-tamano:point_[1]+tamano] 
+
+# Region de la img original
 reg_orig = np.copy(original[point_[0]-tamano:point_[0]+tamano, point_[1]-tamano:point_[1]+tamano, 24])
-print(reg_wm_msk)
+#print(reg_wm_msk)
 
-#reg_wm_msk[:,:] = np.amax(original)
-reg_fcd = gaussian_filter(reg_orig, sigma=1)*reg_wm_msk
+reg_fcd = gaussian_filter(reg_orig, sigma=1)*reg_wm_msk # Region con filtro gaussiano
 
-print(reg_orig)
-print(reg_wm_msk*reg_orig*intensidad)
-wm_intensity = np.where(reg_wm_msk*reg_orig*intensidad>0, reg_wm_msk*reg_orig*intensidad, reg_orig)
-print(wm_intensity)
+print(np.mean(reg_orig))
+print(np.mean(np.copy(orig_flair[point_[0]-tamano:point_[0]+tamano, point_[1]-tamano:point_[1]+tamano, 24])))
+#print(reg_wm_msk*reg_orig*intensidad)
 
-fcd_sim = np.copy(original)
+# Region con FCD simulada por aumento de la intensidad en la materia blanca
+wm_intensity = np.where(reg_wm_msk*reg_orig*intensidad>0, reg_wm_msk*reg_orig*intensidad, reg_orig) 
+#print(wm_intensity)
+
+fcd_sim = np.copy(original) # Copia del volumen original
+fcd_sim_flair = np.copy(orig_flair)
+
+# Sobre la region del volumen original se lleva la region con FCD simulada
 fcd_sim[point_[0]-tamano:point_[0]+tamano, point_[1]-tamano:point_[1]+tamano, 24] = wm_intensity#reg_orig*reg_wm_msk*intensidad#reg_fcd #np.copy(reg_wm_msk)
-print(np.amax(original))
+#print(fcd_sim[point_[0]-tamano:point_[0]+tamano, point_[1]-tamano:point_[1]+tamano, 24])
 
-# filtro
-#
+# Proyeccion de la region simulada sobre la secuencia FLAIR
+fcd_sim_flair[point_[0]-tamano:point_[0]+tamano, point_[1]-tamano:point_[1]+tamano, 24] = wm_intensity
+#print(fcd_sim_flair[point_[0]-tamano:point_[0]+tamano, point_[1]-tamano:point_[1]+tamano, 24])
 
-plot_slice(original[:,:,24], fcd_sim[:,:,24], x)
+plot_slice(original[:,:,24], fcd_sim[:,:,24], fcd_sim_flair[:,:,24])
 
 ## Cargar una FLAIR
+
+## Revisar en la literatura las manifestaciones de FCD en MRI (especialmente aumento de intensidad de materia blanca)
